7d6c5b2913b63a5c47043cc67fa64214f5773a3d
Peter Erwin <erwin@sigmaxi.net>
2016-08-14 13:30:15 +0200

    Final part of initial astropy-affiliated package layout setup

---
+++ ellipsefits/__init__.py
@@ -0,0 +1,18 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This is an Astropy affiliated package for reading, processing, and plotting
+the output of the IRAF stsdas.analysis.isophote.ellipse task (i.e., ellipse
+fits to galaxy isophotes).
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+	pass
+#    from .example_mod import *
